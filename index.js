2a2ff5c86f0108c3085388bce352155c8670d813
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-07 23:44:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const thankMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(thankMessage);
+    }
+  
+    return messages;
+  }
+  
+  console.log(writeCards(["Guadalupe", "Ollie", "Ali"], 'surprise'));
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countDown(counter){
+    while(counter >= 0){
+        console.log(counter--)
+    }
+    
+}
